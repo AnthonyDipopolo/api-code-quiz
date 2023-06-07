b2adbfb4661e4f0246328258680abdd26884b81f
Anthony Di Popolo <ant@anthonys-mbp-3.mynetworksettings.com>
2023-06-07 09:52:17 -0400

    created modal in javascript

--- assets/script.js
+++ assets/script.js
@@ -10,31 +10,89 @@
 //     alert('Times Up');
 //     }
 // }, 1000);
-
+var button = document.querySelector('#btn')
 var htmlTimer = document.querySelector("#time");
 var duration = 60;
 
-// Calculate the target time
-var targetTime = Date.now() + duration * 1000;
 
-// Update the timer every second
-var timer = setInterval(function() {
+button.addEventListener('click', function () {
+  // Calculate the target time
+  var targetTime = Date.now() + duration * 1000;
+
+  // Update the timer every second
+  var timer = setInterval(function () {
     // Get the current time
     var currentTime = Date.now();
-  
+
     // Calculate the remaining time in seconds
     var remainingTime = Math.floor((targetTime - currentTime) / 1000);
-  
+
     // Display the remaining time
     //console.log("Remaining Time:", remainingTime, "seconds");
     htmlTimer.textContent = remainingTime
-  
+
     // Check if the timer has reached 0
     if (remainingTime <= 0) {
       // Timer has finished, stop the interval
       clearInterval(timer);
       htmlTimer.textContent = "Times Up!";
     }
+    //console.log(currentTime)
+
   }, 1000);
 
+});
+
+button.addEventListener('click', function () {
+
+  var h1 = document.querySelector('#question');
+  var p0 = document.querySelector('#answer1');
+  var p1 = document.querySelector('#answer2');
+  var p2 = document.querySelector('#answer3');
+  var p3 = document.querySelector('#answer4');
+  var section = document.querySelector('#modal-content');
+  var modal = document.querySelector('#modal');
+  var hero = document.querySelector('#hero');
+  var button = document.querySelector('#btn');
+  var footer = document.querySelector('#footer');
+  var footerH2 = document.querySelector('#footer-h2')
+  //var header = document.querySelector('#header');
+
+  modal.style.display = "flex";
+  footer.style.display = "flex";
+  hero.style.display = 'none';
+  button.style.display = 'none';
+
+  //header.style.display = 'none';
+  //console.log(modal.section.style.display);
+
+  var data = [
+    {
+      question: 'What does the acronym D.O.M. stand for?',
+      choices: ['Dog Only Members', 'Document Object Model', 'Dancing On Mercury', 'Document On Memory'],
+      answer: 'Document Object Model'
+    }
+  ];
+
+  for (var index = 0; index < data.length; index++) {
+    var questionObj = data[index];
   
+  h1.innerText = questionObj.question;
+  p0.innerText = questionObj.choices[0];
+  p1.innerText = questionObj.choices[1];
+  p2.innerText = questionObj.choices[2];
+  p3.innerText = questionObj.choices[3];
+  footer.innerText = questionObj.answer;
+
+  section.append(h1);
+  section.append(p0);
+  section.append(p1);
+  section.append(p2);
+  section.append(p3);
+  footer.append(footerH2);
+
+  console.log(footer);
+
+  console.log(questionObj.answer);
+  }
+});
